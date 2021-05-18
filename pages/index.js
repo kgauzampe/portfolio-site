6e9ed8afa2b990e32a2414c62e806d95817eefd0
kgauzampe <kgauza.mpe@gmail.com>
2021-05-18 17:37:58 +0200

    Title changed

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
               </Typography>
 
               <Typography component="h4" variant="h4">
-                Web developer
+                Junior Software Engineer
               </Typography>
 
               <Button variant="contained" className="aboutBtn" href="/about">
